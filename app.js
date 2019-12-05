a7b56b52f6f84760ba6d8758aab53a977e4b7f8d
zilb88 <khan.anton.01@gmail.com>
2019-12-05 14:43:41 +0200

    add js file

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementsByClassName('ba-breadcrumbs_item')[1].id = 'sssss';
+sssss.style.border = 'solid red';
+document.getElementsByClassName('ba-breadcrumbs')[0].id = 'sssss2';
+sssss2.style.border = 'solid red';
+
+document.getElementsByTagName('span')[6].id = 'zagolovok';
+document.getElementsByTagName('span')[7].id = 'zagolovok';
+document.getElementsByTagName('span')[8].id = 'zagolovok';
+document.getElementById('zagolovok').style.color = 'red';
+
+
+document.getElementsByClassName('ba-team-member_img')[0].id = 'imgs';
+document.getElementsByClassName('ba-team-member_img')[1].id = 'imgs';
+document.getElementsByClassName('ba-team-member_img')[2].id = 'imgs';
+document.getElementsByClassName('ba-team-member_img')[3].id = 'imgs';
+document.getElementsByClassName('ba-team-member_img')[4].id = 'imgs';
+document.getElementsByClassName('ba-team-member_img')[5].id = 'imgs';
+document.getElementsByClassName('ba-team-member_img')[6].id = 'imgs';
+document.getElementsByClassName('ba-team-member_img')[7].id = 'imgs';
+
+let el = document.getElementById('imgs');
+el.src = 'https://www.shorturl.at/nsuFK';
